7a2264cb53cb80de67e6b688e933db5044551fb3
malvikabadal <malvikabadal@hotmail.com>
2024-11-14 11:37:02 +0100

    added docstrings

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -1,3 +1,5 @@
+# make a list of numbers between 0 and 2pi and of the tan or the sin of these numbers
+
 import click
 import numpy as np
 import pandas as pd
@@ -9,29 +11,41 @@ def cmd_group():
     pass
 
 
+# make a subcommand sin
 @cmd_group.command()
 @click.option(
     "-n",
     "--number",
     default=10,
-    help="Amount of steps.",
-    show_default=True,  # show default in help
+    help="Amount of steps between 0 and 2pi.",
+    show_default=True,
 )
 def sin(number):
+    """Make a list of numbers between 0 and 2pi and of the sin of these numbers.
+
+    Args:
+        number (int): amount of steps between 0 and 2pi
+    """
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
 
+# make a subcommand tan
 @cmd_group.command()
 @click.option(
     "-n",
     "--number",
     default=10,
-    help="Amount of steps.",
-    show_default=True,  # show default in help
+    help="Amount of steps between 0 and 2pi.",
+    show_default=True,
 )
 def tan(number):
+    """Make a list of numbers between 0 and 2pi and of the tan of these numbers.
+
+    Args:
+        number (int): amount of steps between 0 and 2pi
+    """
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
