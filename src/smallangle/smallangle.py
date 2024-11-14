10f46019cf542a674a757ba40e394bd1c434ee14
malvikabadal <malvikabadal@hotmail.com>
2024-11-14 11:26:05 +0100

    made commands
    made a subcommand of functions sin and tan, made a command smallangle

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -1,15 +1,36 @@
 import click
 import numpy as np
-from numpy import pi
 import pandas as pd
+from numpy import pi
 
 
+@click.group()
+def cmd_group():
+    pass
+
+
+@cmd_group.command()
+@click.option(
+    "-n",
+    "--number",
+    default=10,
+    help="Amount of steps.",
+    show_default=True,  # show default in help
+)
 def sin(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
 
+@cmd_group.command()
+@click.option(
+    "-n",
+    "--number",
+    default=10,
+    help="Amount of steps.",
+    show_default=True,  # show default in help
+)
 def tan(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
@@ -17,4 +38,4 @@ def tan(number):
 
 
 if __name__ == "__main__":
-    sin(10)
+    cmd_group()
